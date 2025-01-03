7d9b4fcaa1a23e3f86536ca8fafa0c349dac798c
HoaNP <7907509+HoaNP@users.noreply.github.com>
2025-01-03 14:08:36 +0000

    Update map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,692 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Graz, Austria",
+    47.0708678,
+    15.4382786
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Vienna, Austria",
+    48.2083537,
+    16.3725042
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Tal, Austria",
+    47.5211115,
+    15.0771712
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Hallstatt, Austria",
+    47.5621698,
+    13.6486697
+  ],
+  [
+    "Bruck an der Mur, Austria",
+    47.4121987,
+    15.2721668
+  ],
+  [
+    "Obertraun, Austria",
+    47.5580272,
+    13.685288
+  ],
+  [
+    "Klagenfurt, Austria",
+    46.623943,
+    14.3075976
+  ],
+  [
+    "Antwerp, Belgium",
+    51.2211097,
+    4.3997081
+  ],
+  [
+    "Leuven, Belgium",
+    50.879202,
+    4.7011675
+  ],
+  [
+    "Brussels, Belgium",
+    50.8465573,
+    4.351697
+  ],
+  [
+    "Schaerbeek, Belgium",
+    50.8676041,
+    4.3737121
+  ],
+  [
+    "Louvain-la-Neuve, Belgium",
+    50.6741689,
+    4.613790587187648
+  ],
+  [
+    "Blankenberge, Belgium",
+    51.3112589,
+    3.1323429
+  ],
+  [
+    "Ghent, Belgium",
+    51.0538286,
+    3.7250121
+  ],
+  [
+    "Turnhout, Belgium",
+    51.3286495,
+    4.929594325676078
+  ],
+  [
+    "Bruges, Belgium",
+    51.2085526,
+    3.226772
+  ],
+  [
+    "Phnom Penh, Cambodia",
+    11.5730391,
+    104.857807
+  ],
+  [
+    "Hekou, China",
+    22.7405358,
+    103.8243528
+  ],
+  [
+    "Zagreb, Croatia",
+    45.8130967,
+    15.9772795
+  ],
+  [
+    "Breclav, Czechia",
+    48.7594008,
+    16.8813137
+  ],
+  [
+    "Mikulov, Czechia",
+    48.8068577,
+    16.6383914
+  ],
+  [
+    "Brno, Czechia",
+    49.1922443,
+    16.6113382
+  ],
+  [
+    "Prague, Czechia",
+    50.0596288,
+    14.446459273258009
+  ],
+  [
+    "Valtice, Czechia",
+    48.7412949,
+    16.755758
+  ],
+  [
+    "Copenhagen, Denmark",
+    55.6867243,
+    12.5700724
+  ],
+  [
+    "Rennes, France",
+    48.1113387,
+    -1.6800198
+  ],
+  [
+    "Paris, France",
+    48.8534951,
+    2.3483915
+  ],
+  [
+    "Caen, France",
+    49.1813403,
+    -0.3635615
+  ],
+  [
+    "Vitre, France",
+    48.1244503,
+    -1.214331
+  ],
+  [
+    "Dinan, France",
+    48.4539775,
+    -2.047687
+  ],
+  [
+    "Nantes, France",
+    47.2186371,
+    -1.5541362
+  ],
+  [
+    "Nice, France",
+    43.7009358,
+    7.2683912
+  ],
+  [
+    "Cannes, France",
+    43.5515198,
+    7.0134418
+  ],
+  [
+    "Menton, France",
+    43.7753495,
+    7.5029213
+  ],
+  [
+    "Saint-Malo, France",
+    48.649518,
+    -2.0260409
+  ],
+  [
+    "Dinard, France",
+    48.6320379,
+    -2.0580178
+  ],
+  [
+    "Nancy, France",
+    48.6937223,
+    6.1834097
+  ],
+  [
+    "Mondeville, France",
+    49.1771038,
+    -0.3202231
+  ],
+  [
+    "Etretat, France",
+    49.7074621,
+    0.2031905
+  ],
+  [
+    "Cherbourg, France",
+    49.51271975,
+    -1.5755441090380597
+  ],
+  [
+    "Pau, France",
+    43.2957547,
+    -0.3685668
+  ],
+  [
+    "Toulouse, France",
+    43.6044622,
+    1.4442469
+  ],
+  [
+    "Ussat, France",
+    42.83029,
+    1.6194
+  ],
+  [
+    "Tarascon-sur-Ariege, France",
+    42.8458016,
+    1.6067586
+  ],
+  [
+    "Lourdes, France",
+    43.0940904,
+    -0.0464975
+  ],
+  [
+    "Saint-Pe-de-Bigorre, France",
+    43.1034783,
+    -0.1589287
+  ],
+  [
+    "Laruns, France",
+    42.9882368,
+    -0.4266324
+  ],
+  [
+    "Saint-Brieuc, France",
+    48.5141134,
+    -2.7603283
+  ],
+  [
+    "Plevenon, France",
+    48.6548909,
+    -2.3315588
+  ],
+  [
+    "Lyon, France",
+    45.7578137,
+    4.8320114
+  ],
+  [
+    "Rouen, France",
+    49.4404591,
+    1.0939658
+  ],
+  [
+    "Bordeaux, France",
+    44.841225,
+    -0.5800364
+  ],
+  [
+    "Lorient, France",
+    47.7477336,
+    -3.3660907
+  ],
+  [
+    "Lille, France",
+    50.6365654,
+    3.0635282
+  ],
+  [
+    "Fougeres, France",
+    48.3503362,
+    -1.1958526
+  ],
+  [
+    "Thionville, France",
+    49.3579272,
+    6.1675872
+  ],
+  [
+    "Mont Saint-Michel, France",
+    48.6359541,
+    -1.511459954959514
+  ],
+  [
+    "Grenoble, France",
+    45.1875602,
+    5.7357819
+  ],
+  [
+    "Annecy, France",
+    45.8992348,
+    6.1288847
+  ],
+  [
+    "Vannes, France",
+    47.6586772,
+    -2.7599079
+  ],
+  [
+    "Limoges, France",
+    45.8354243,
+    1.2644847
+  ],
+  [
+    "Saint-Cast-le-Guildo, France",
+    48.6241288,
+    -2.2617846
+  ],
+  [
+    "Brest, France",
+    48.3905283,
+    -4.4860088
+  ],
+  [
+    "Uzerche, France",
+    45.4243315,
+    1.5637278
+  ],
+  [
+    "Strasbourg, France",
+    48.584614,
+    7.7507127
+  ],
+  [
+    "Colmar, France",
+    48.0777517,
+    7.3579641
+  ],
+  [
+    "Rocamadour, France",
+    44.7995467,
+    1.6181714
+  ],
+  [
+    "Montpellier, France",
+    43.6112422,
+    3.8767337
+  ],
+  [
+    "Nimes, France",
+    43.8374249,
+    4.3600687
+  ],
+  [
+    "Marseille, France",
+    43.2961743,
+    5.3699525
+  ],
+  [
+    "Cassis, France",
+    43.2140359,
+    5.5396318
+  ],
+  [
+    "Rougon, France",
+    43.7989923,
+    6.4021491
+  ],
+  [
+    "Valensole, France",
+    43.8379283,
+    5.9839867
+  ],
+  [
+    "Avignon, France",
+    43.9492493,
+    4.8059012
+  ],
+  [
+    "Vaison-la-Romaine, France",
+    44.2407641,
+    5.075737
+  ],
+  [
+    "Poitiers, France",
+    46.5802596,
+    0.340196
+  ],
+  [
+    "Reims, France",
+    49.2577886,
+    4.031926
+  ],
+  [
+    "Arcachon, France",
+    44.6616428,
+    -1.1700018
+  ],
+  [
+    "Berlin, Germany",
+    52.510885,
+    13.3989367
+  ],
+  [
+    "Bochum, Germany",
+    51.4818111,
+    7.2196635
+  ],
+  [
+    "Dresden, Germany",
+    51.0493286,
+    13.7381437
+  ],
+  [
+    "Frankfurt, Germany",
+    50.1106444,
+    8.6820917
+  ],
+  [
+    "Potsdam, Germany",
+    52.4009309,
+    13.0591397
+  ],
+  [
+    "Brandenburg, Germany",
+    52.8455492,
+    13.2461296
+  ],
+  [
+    "Fira, Greece",
+    36.4149227,
+    25.4324527
+  ],
+  [
+    "Oia, Greece",
+    36.4622122,
+    25.3757257
+  ],
+  [
+    "Athens, Greece",
+    37.9755648,
+    23.7348324
+  ],
+  [
+    "Budapest, Hungary",
+    47.4978789,
+    19.0402383
+  ],
+  [
+    "Batam, Indonesia",
+    1.1030815,
+    104.0383696
+  ],
+  [
+    "Rome, Italy",
+    41.8933203,
+    12.4829321
+  ],
+  [
+    "Venice, Italy",
+    45.4371908,
+    12.3345898
+  ],
+  [
+    "Milan, Italy",
+    45.4641943,
+    9.1896346
+  ],
+  [
+    "Varenna, Italy",
+    46.0099785,
+    9.2831593
+  ],
+  [
+    "Vientiane, Laos",
+    17.9640988,
+    102.6133707
+  ],
+  [
+    "Luxembourg, Luxembourg",
+    49.6112768,
+    6.129799
+  ],
+  [
+    "Malacca, Malaysia",
+    2.3293744,
+    102.2880962
+  ],
+  [
+    "Kuala Lumpur, Malaysia",
+    3.1516964,
+    101.6942371
+  ],
+  [
+    "Johor Bahru, Malaysia",
+    1.4581986,
+    103.7649059
+  ],
+  [
+    "Sliema, Malta",
+    35.9127609,
+    14.5020708
+  ],
+  [
+    "Msida, Malta",
+    35.8956943,
+    14.4891829
+  ],
+  [
+    "Valletta, Malta",
+    35.8989979,
+    14.5136607
+  ],
+  [
+    "Rabat, Malta",
+    35.8818906,
+    14.3988213
+  ],
+  [
+    "Qrendi, Malta",
+    35.8343586,
+    14.4578698
+  ],
+  [
+    "Monaco, Monaco",
+    43.7323492,
+    7.4276832
+  ],
+  [
+    "Leiden, Netherlands",
+    52.1594747,
+    4.4908843
+  ],
+  [
+    "Amsterdam, Netherlands",
+    52.3730796,
+    4.8924534
+  ],
+  [
+    "The Hague, Netherlands",
+    52.07494555,
+    4.2696802205364515
+  ],
+  [
+    "Lisse, Netherlands",
+    52.2576098,
+    4.5630329
+  ],
+  [
+    "Warsaw, Poland",
+    52.2337172,
+    21.071432235636493
+  ],
+  [
+    "Krakow, Poland",
+    50.0469432,
+    19.997153435836697
+  ],
+  [
+    "Lisbon, Portugal",
+    38.7077507,
+    -9.1365919
+  ],
+  [
+    "Sintra, Portugal",
+    38.79846,
+    -9.3881
+  ],
+  [
+    "Cascais, Portugal",
+    38.6968919,
+    -9.4204495
+  ],
+  [
+    "Singapore, Singapore",
+    1.2899175,
+    103.8519072
+  ],
+  [
+    "Bratislava, Slovakia",
+    48.1516988,
+    17.1093063
+  ],
+  [
+    "Maribor, Slovenia",
+    46.5576439,
+    15.6455854
+  ],
+  [
+    "Bled, Slovenia",
+    46.3684202,
+    14.1100595
+  ],
+  [
+    "Blejska Dobrava, Slovenia",
+    46.412041,
+    14.0952928
+  ],
+  [
+    "Barcelona, Spain",
+    41.3828939,
+    2.1774322
+  ],
+  [
+    "Frontera del Portalet, Spain",
+    42.8052893,
+    -0.4183895
+  ],
+  [
+    "Lausanne, Switzerland",
+    46.5218269,
+    6.6327025
+  ],
+  [
+    "Bern, Switzerland",
+    46.9484742,
+    7.4521749
+  ],
+  [
+    "Belp, Switzerland",
+    46.88978,
+    7.498388
+  ],
+  [
+    "Brienz, Switzerland",
+    46.7560001,
+    8.0303006
+  ],
+  [
+    "Interlaken, Switzerland",
+    46.6855231,
+    7.8585139
+  ],
+  [
+    "Spiez, Switzerland",
+    46.6919473,
+    7.6774655
+  ],
+  [
+    "Grindelwald, Switzerland",
+    46.6242733,
+    8.0367462
+  ],
+  [
+    "Lauterbrunnen, Switzerland",
+    46.5939043,
+    7.9078016
+  ],
+  [
+    "Thun, Switzerland",
+    46.7582827,
+    7.628086
+  ],
+  [
+    "Lucerne, Switzerland",
+    47.0505452,
+    8.3054682
+  ],
+  [
+    "Arth, Switzerland",
+    47.0644551,
+    8.5238673
+  ],
+  [
+    "Zurich, Switzerland",
+    47.3744489,
+    8.5410422
+  ],
+  [
+    "Geneva, Switzerland",
+    46.2017559,
+    6.1466014
+  ],
+  [
+    "Taipei, Taiwan",
+    25.0375198,
+    121.5636796
+  ],
+  [
+    "Bangkok, Thailand",
+    13.7524938,
+    100.4935089
+  ],
+  [
+    "Hanoi, Vietnam",
+    21.0283334,
+    105.854041
+  ],
+  [
+    "Thai Nguyen, Vietnam",
+    21.6498502,
+    105.8351394
+  ],
+  [
+    "Nam Dinh, Vietnam",
+    20.2686476,
+    106.2289075
   ]
 ];
